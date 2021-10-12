8419a2467b1723c0794c7bd8ebc213d09b2ee044
CuteGoth <privetik065@gmail.com>
2021-10-12 13:37:41 +0500

    create models and repositories

--- src/main/java/com/example/springwebapp/controller/Home.java
+++ src/main/java/com/example/springwebapp/controller/Home.java
@@ -6,6 +6,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpSession;
+import javax.swing.*;
 import java.util.ArrayList;
 import java.util.Date;
 
@@ -26,10 +27,14 @@ public class Home {
         String sid = session.getId();
         model.addAttribute("sid", sid);
 
-        ArrayList<Product> products = new ArrayList<>();
-        products.add(new Product("Old product", 1, new Date()));
-        products.add(new Product("New product", 2, new Date()));
-        model.addAttribute("products", products);
+//        ArrayList<Product> products = new ArrayList<>();
+//        products.add(new Product("КОФЕ МОЛОТЫЙ TASTY COFFEE БРАУН КЭНДИ 250Г", 300, new Date()));
+//        products.add(new Product("КОФЕ МОЛОТЫЙ TASTY COFFEE БАРИСТА 250Г", 320, new Date()));
+//        products.add(new Product("КОФЕ МОЛОТЫЙ TASTY COFFEE БЛЭК КЭНДИ 250Г", 300, new Date()));
+//        products.add(new Product("КОФЕ В ЗЕРНАХ TASTY COFFEE БЛЭК КЭНДИ 250Г", 250, new Date()));
+//        products.add(new Product("КОФЕ В ЗЕРНАХ TASTY COFFEE БАРИСТА 250Г", 290, new Date()));
+//        products.add(new Product("КОФЕ В ЗЕРНАХ TASTY COFFEE КОЛУМБИЯ УИЛА 250Г", 350, new Date()));
+//        model.addAttribute("products", products);
         return "index";
     }
 }
--- src/main/java/com/example/springwebapp/model/Product.java
+++ src/main/java/com/example/springwebapp/model/Product.java
@@ -1,41 +1,47 @@
 package com.example.springwebapp.model;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-import java.util.Date;
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.Set;
 
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "products", schema = "coffeshop")
 public class Product {
 
+    @Id
+    @Column(name = "product_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column(name = "description")
     private String description;
-    private Integer price;
-    private Date availableFrom;
+    @Column(name = "price")
+    private int price;
+    @Column(name = "date")
+    private LocalDateTime date;
+    @Column(name = "count")
+    private int count;
+    @Column(name = "image")
+    private String image;
 
-    public Product(final String description, final Integer price, final Date availableFrom) {
-        this.description = description;
-        this.price = price;
-        this.availableFrom = availableFrom;
-    }
+//    множеству продуктов соответствует одна категория
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "category_id")
+    private Category category;
 
-    public Date getAvailableFrom() {
-        return this.availableFrom;
-    }
 
-    public void setAvailableFrom(final Date availableFrom) {
-        this.availableFrom = availableFrom;
-    }
-
-    public String getDescription() {
-        return this.description;
-    }
-
-    public void setDescription(final String description) {
-        this.description = description;
-    }
-
-    public Integer getPrice() {
-        return this.price;
-    }
-
-    public void setPrice(final Integer price) {
-        this.price = price;
-    }
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(name = "ordered_products", schema = "coffeshop",
+            joinColumns = @JoinColumn(name = "product_id"),
+            inverseJoinColumns = @JoinColumn(name = "order_id"))
+    private Set<Order> orders;
 
 }
